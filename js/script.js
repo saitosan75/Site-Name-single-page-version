af4fb6988fcdd3766bcc6110cdee61b569eb1734
saitosan75 <saitosan@hawaii.edu>
2018-08-28 12:48:24 -1000

    added initial site files

---
+++ js/script.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+  //TOGGLE MENU NAV
+  var menubutton = document.querySelectorAll('.menu-button');
+  var i;
+  for (i = 0; i < menubutton.length; i++) {
+      menubutton[i].onclick = function(){
+          this.classList.toggle("expanded");
+      };
+  } 
+    
+ //STICKTY NAV SCROLL LINKS FOR SINGLE PAGE SITES
+ var stickynavlinks = document.querySelectorAll('.sticky nav a');
+ var j;
+ for (j = 0; j < stickynavlinks.length; j++) {
+     stickynavlinks[j].onclick = function(){
+        var stickymenu = document.querySelectorAll('.sticky .menu-button');
+        var k;
+        for (k = 0; k < stickymenu.length; k++) {
+            stickymenu[k].classList.remove("expanded");
+        } 
+     };
+ } 
+});
